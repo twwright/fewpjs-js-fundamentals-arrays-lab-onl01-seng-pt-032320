e46e2dc7e8089b710420a89d3b3aaaeec0f9c0cf
Thomas Wright <twwright@gmail.com>
2020-09-18 18:39:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const arr = ["Milo", "Otis", "Garfield"];
 
 const append = arr;
-console.log(append);
+// console.log(append);
 const prepend = arr;
-console.log(prepend);
+// console.log(prepend);
 const removeLast = arr;
-console.log(removeLast);
+// console.log(removeLast);
 const removeFirst = arr;
-console.log(removeFirst);
+// console.log(removeFirst);
 
 append.push("Odie");
-console.log(append);
-console.log(arr);
+// console.log(append);
+// console.log(arr);
 prepend.unshift("Odie");
-console.log(prepend);
-console.log(arr);
+// console.log(prepend);
+// console.log(arr);
 removeLast.pop();
-console.log(removeLast);
-console.log(arr);
+// console.log(removeLast);
+// console.log(arr);
 removeFirst.shift();
-console.log(removeFirst);
-console.log(arr);
+// console.log(removeFirst);
+// console.log(arr);
