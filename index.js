7cdd1599f23aa5d4bffc7ef45a91f8041c8de057
Thomas Wright <twwright@gmail.com>
2020-09-18 17:49:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const removeLast = arr;
 const removeFirst = arr;
 
 append.push("Odie");
-
+console.log(append);
+console.log(arr);
 prepend.unshift("Odie");
-
+console.log(prepend);
+console.log(arr);
 removeLast.pop();
-
+console.log(removeLast);
+console.log(arr);
 removeFirst.shift();
+console.log(removeFirst);
+console.log(arr);
