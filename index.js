50790740285b2cb21733849f25df0aa472565241
Thomas Wright <twwright@gmail.com>
2020-09-18 18:45:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution here!
+const arr = ["Milo", "Otis", "Garfield"];
+
+const append = ["Milo", "Otis", "Garfield"];
+// console.log(append);
+const prepend = ["Milo", "Otis", "Garfield"];
+// console.log(prepend);
+const removeLast = ["Milo", "Otis", "Garfield"];
+// console.log(removeLast);
+const removeFirst = ["Milo", "Otis", "Garfield"];
+// console.log(removeFirst);
+
+append.push("Odie");
+// console.log(append);
+// console.log(arr);
+prepend.unshift("Odie");
+// console.log(prepend);
+// console.log(arr);
+removeLast.pop();
+// console.log(removeLast);
+// console.log(arr);
+removeFirst.shift();
+// console.log(removeFirst);
+// console.log(arr);
